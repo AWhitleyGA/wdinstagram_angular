7cf4b20c95e52591fbaf0706c8c6ee550237a919
Andrew Whitley <awhitley1233@gmail.com>
2016-10-26 09:02:07 -0400

    finished index and new views/functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,61 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+angular
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("GramIndexController", [
+    "GramFactory",
+    GramIndexControllerFunction
+  ])
+  .controller("GramNewController", [
+    "GramFactory",
+    "$state",
+    GramNewControllerFunction
+  ])
+  .factory("GramFactory", [
+    "$resource",
+    GramFactoryFunction
+  ])
+
+  function GramFactoryFunction($resource) {
+    return $resource("http://localhost:3000/entries")
+  }
+
+  function GramIndexControllerFunction(GramFactory) {
+    this.grams = GramFactory.query()
+    this.display = console.log(this.grams)
+  }
+
+  function GramNewControllerFunction(GramFactory, $state) {
+    this.gram = new GramFactory()
+    this.create = function() {
+      GramFactory.save(this.gram, function() {
+        $state.go("gramIndex")
+      })
+    }
+  }
+
+  function RouterFunction($stateProvider) {
+    $stateProvider
+    .state("gramIndex", {
+      url: "/grams",
+      templateUrl: "js/ng-views/index.html",
+      controller: "GramIndexController",
+      controllerAs: "vm"
+    })
+    .state("gramNew", {
+      url: "/grams/new",
+      templateUrl: "js/ng-views/new.html",
+      controller: "GramNewController",
+      controllerAs: "vm"
+    })
+  }
+
 })();
